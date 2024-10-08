48fe0a8fd958529a1897484e69f8bf4739f7ed68
Kevin Carr <kwcarr12@gmail.com>
2024-10-07 22:53:31 -0400

    Refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ using namespace std::chrono;
 void printSortedArray(vector<int> &arr);
 void printUnsortedArray(vector<int> &arr);
 void printArray(vector<int> &arr);
+static void benchmarkingSortingAlgorithms(int length);
+static void practiceSortingAlgorithms(int length);
 
 int main()
 {
@@ -22,15 +24,22 @@ int main()
     cin >> length;
     cout << endl;
 
+    benchmarkingSortingAlgorithms(length);
+
+    cout << endl;
+
+    practiceSortingAlgorithms(length);
+
+    return 0;
+}
+
+/************************************************************
+ * @brief Benchmarks the sorting algorithms
+ ***********************************************************/
+static void benchmarkingSortingAlgorithms(int length)
+{
     int min = 1;
     int max = 100;
-
-    bool bubbleSortStatus = false;
-    bool insertionSortStatus = false;
-    bool selectionSortStatus = false;
-    bool mergeSortStatus = false;
-    bool quickSortStatus = false;
-    bool heapSortStatus = false;
 
     vector<int> randArray = generateRandomIntArray(length, min, max);
     auto start = high_resolution_clock::now();
@@ -38,115 +47,72 @@ int main()
 
     // BUBBLE SORT
     vector<int> randArray1 = randArray;
-    cout << "Here's an example of Bubble Sort on the following array:" << endl;
-
-    printUnsortedArray(randArray1);
     start = high_resolution_clock::now();
     bubbleSort(randArray1);
     stop = high_resolution_clock::now();
-    printSortedArray(randArray1);
     auto bubbleTime = duration_cast<microseconds>(stop - start);
-    cout << "Time to complete: " << bubbleTime.count() << " microseconds" << endl;
-    bubbleSortStatus = checkIntsAscending(randArray1);
-#if 0 
-    checkStatus(bubbleSortStatus);
-#endif
-
-    cout << endl;
 
     // INSERTION SORT
     vector<int> randArray2 = randArray;
-    cout << "Here's an example of Insertion Sort to sort the same array:" << endl;
-
-    printUnsortedArray(randArray2);
     start = high_resolution_clock::now();
     insertionSort(randArray2);
     stop = high_resolution_clock::now();
-    printSortedArray(randArray2);
     auto insertionTime = duration_cast<microseconds>(stop - start);
-    cout << "Time to complete: " << insertionTime.count() << " microseconds" << endl;
-    insertionSortStatus = checkIntsAscending(randArray2);
-#if 0
-    checkStatus(insertionSortStatus);
-#endif
-
-    cout << endl;
 
     // SELECTION SORT
     vector<int> randArray3 = randArray;
-    cout << "Here's an example of Selection Sort to sort the same array:" << endl;
-
-    printUnsortedArray(randArray3);
     start = high_resolution_clock::now();
     selectionSort(randArray3);
     stop = high_resolution_clock::now();
-    printSortedArray(randArray3);
     auto selectionTime = duration_cast<microseconds>(stop - start);
-    cout << "Time to complete: " << selectionTime.count() << " microseconds" << endl;
-    selectionSortStatus = checkIntsAscending(randArray3);
-#if 0 
-    checkStatus(selectionSortStatus);
-#endif
 
     // MERGE SORT
     vector<int> randArray4 = randArray;
-    cout << "Merge Sort:" << endl;
-    printUnsortedArray(randArray4);
     start = high_resolution_clock::now();
     mergeSort(randArray4, 0, randArray4.size() - 1);
     stop = high_resolution_clock::now();
-    printSortedArray(randArray4);
     auto mergeTime = duration_cast<microseconds>(stop - start);
-    cout << "Time to complete: " << mergeTime.count() << " microseconds" << endl;
-    mergeSortStatus = checkIntsAscending(randArray4);
 
     // QUICK SORT
     vector<int> randArray5 = randArray;
-    cout << "Quick Sort:" << endl;
-    printUnsortedArray(randArray5);
     start = high_resolution_clock::now();
     quickSort(randArray5);
     stop = high_resolution_clock::now();
-    printSortedArray(randArray5);
     auto quickSortTime = duration_cast<microseconds>(stop - start);
-    cout << "Time to complete: " << quickSortTime.count() << " microseconds" << endl;
-    quickSortStatus = checkIntsAscending(randArray5);
 
     // HEAP SORT
     vector<int> randArray6 = randArray;
-    cout << "Heap Sort:" << endl;
-    printUnsortedArray(randArray6);
     start = high_resolution_clock::now();
     heapSort(randArray6);
     stop = high_resolution_clock::now();
-    printSortedArray(randArray6);
     auto heapSortTime = duration_cast<microseconds>(stop - start);
-    cout << "Time to complete: " << heapSortTime.count() << " microseconds" << endl;
-    heapSortStatus = checkIntsAscending(randArray6);
-
-    cout << endl;
 
     cout << "     Benchmark Summary:     " << endl;
     cout << "============================" << endl;
-    cout << "Bubble Sort Time: " << bubbleTime.count() << " microseconds ";
-    checkStatus(bubbleSortStatus);
-    cout << "Insertion Sort Time: " << insertionTime.count() << " microseconds ";
-    checkStatus(insertionSortStatus);
-    cout << "Selection Sort Time: " << selectionTime.count() << " microseconds ";
-    checkStatus(selectionSortStatus);
-    cout << "Merge Sort Time: " << mergeTime.count() << " microseconds ";
-    checkStatus(mergeSortStatus);
-    cout << "Quick Sort Time: " << quickSortTime.count() << " microseconds ";
-    checkStatus(quickSortStatus);
-    cout << "Heap Sort Time: " << heapSortTime.count() << " microseconds ";
-    checkStatus(heapSortStatus);
+    cout << "Bubble Sort Time: " << bubbleTime.count() << " microseconds " << endl;
+    cout << "Insertion Sort Time: " << insertionTime.count() << " microseconds " << endl;
+    cout << "Selection Sort Time: " << selectionTime.count() << " microseconds " << endl;
+    cout << "Merge Sort Time: " << mergeTime.count() << " microseconds " << endl;
+    cout << "Quick Sort Time: " << quickSortTime.count() << " microseconds " << endl;
+    cout << "Heap Sort Time: " << heapSortTime.count() << " microseconds " << endl;
 
-    cout << endl;
+    return;
+}
 
-    cout << "    Practice Algorithms    " << endl;
-    cout << "===========================" << endl;
+/************************************************************
+ * @brief Function groups practice function calls together
+ ***********************************************************/
+static void practiceSortingAlgorithms(int length)
+{
+    int min = 0;
+    int max = 100;
+    vector<int> randArray = generateRandomIntArray(length, min, max);
+    auto start = high_resolution_clock::now();
+    auto stop = high_resolution_clock::now();
 
     // PRACTICE
+    cout << "    PRACTICE RESULTS    " << endl;
+    cout << "========================" << endl;
 
     vector<int> randArrayA = randArray;
     cout << "Practice Bubble Sort: ";
@@ -253,12 +219,15 @@ int main()
     }
     else
     {
-        checkStatus(pQuickSortStatus);
+        checkStatus(pHeapSortStatus);
     }
 
-    return 0;
+    return;
 }
 
+/************************************************************
+ * @brief Prints the array with an unsorted heading
+ ***********************************************************/
 void printUnsortedArray(vector<int> &arr)
 {
     cout << "Unsorted Array: ";
@@ -268,6 +237,9 @@ void printUnsortedArray(vector<int> &arr)
     return;
 }
 
+/************************************************************
+ * @brief Prints the array with a Sorted heading
+ ***********************************************************/
 void printSortedArray(vector<int> &arr)
 {
     cout << "Sorted Array: ";
@@ -277,6 +249,9 @@ void printSortedArray(vector<int> &arr)
     return;
 }
 
+/************************************************************
+ * @brief Prints the array
+ ***********************************************************/
 void printArray(vector<int> &arr)
 {
     cout << "[ ";
